08cfceabbdfbbd5f62bbb7b97ac56734263bd3ec
Sumaiya Siddika <to.sumaiyasiddika@gmail.com>
2024-04-23 11:45:36 +0600

    build

--- dist/modules.asset.php
+++ dist/modules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8b7e860e1a61abbb23f6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '0698306a4aaf53f21cb8');
